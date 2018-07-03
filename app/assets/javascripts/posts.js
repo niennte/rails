9c53f958b3d78053847f5685811f4783fb4abc46
zencam@imac <zen.camera.project@gmail.com>
2018-07-03 18:30:41 -0400

    Implement front end map using MapBox.

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,94 +1,149 @@
-window.onload = function() {
+$(document).ready(function() {
 
-    mapboxgl.accessToken = 'pk.eyJ1Ijoibmllbm50ZSIsImEiOiJjamo0ajE5aDgxajJhM2twZzB4cWRxNXFzIn0.wrH52IDoERpZGasQNOjUXg';
 
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/outdoors-v10',
-        center: [-81.55, 46.00],
-        zoom: 10.15
-    });
+    const initializeMap = function (data) {
 
-    map.on('load', function () {
+        mapboxgl.accessToken = 'pk.eyJ1Ijoibmllbm50ZSIsImEiOiJjamo0ajE5aDgxajJhM2twZzB4cWRxNXFzIn0.wrH52IDoERpZGasQNOjUXg';
 
-        // Add a layer showing the places.
-        map.addLayer({
-            "id": "places",
-            "type": "symbol",
-            "source": {
-                "type": "geojson",
-                "data": {
-                    "type": "FeatureCollection",
-                    "features": [{
-                        "type": "Feature",
-                        "properties": {
-                            "description": "<strong>Kayaking in Killarney</strong><p>Seen when kayaking in Killarney</p><img src='http://localhost:3000/system/posts/photos/000/000/005/thumb/DSCN6410.jpg?1530313327' />",
-                            "icon": "attraction"
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [-81.40155899999999, 46.0119406]
-                        }
-                    }, {
-                        "type": "Feature",
-                        "properties": {
-                            "description": "<strong>Hiking in Killarney</strong><p>Chikanishing trail in Killarney provincial park.</p><img src='http://localhost:3000/system/posts/photos/000/000/006/thumb/DSCN5864.JPG?1530313304' />",
-                            "icon": "attraction"
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [-81.4154405, 46.0008219]
-                        }
-                    }, {
-                        "type": "Feature",
-                        "properties": {
-                            "description": "<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a Big Backyard Beach Bash and Wine Fest on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.grill hot dogs.</p>",
-                            "icon": "bar"
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [-77.090372, 38.881189]
-                        }
-                    }]
+        var response = $.parseJSON(data);
+
+        // convert response to expected geoJSON
+        const posts = response.map( function(feature) {
+            feature.properties.icon = 'attraction';
+            feature.properties.description = feature.properties.popupContent;
+            delete feature.properties.popupContent;
+            return feature;
+        });
+
+
+        const extractCoordinates = function(posts) {
+            return posts
+                .map(function(feature) {
+                    return feature.geometry.coordinates;
+                }) // omit posts with no coordinates
+                .filter(function(coordinates) {
+                    return coordinates[0] && coordinates[1];
+                });
+        };
+
+        // margin optional param allows to leave space between objects and map edges
+        // using degrees and latitude and longitude as unit
+        // while longitude may be 0, horizontal margins are seldom the issue
+        // unless a lot of users take a lot of photos at the poles :)
+        const mapBounds = function(coordinatePairs, margin) {
+            margin = margin || 0.5;
+            const bounds = coordinatePairs
+                .reduce(function(bounds, coords) {
+                    return [
+                        [Math.min(bounds[0][0], coords[0]), Math.min(bounds[0][1], coords[1])],
+                        [Math.max(bounds[1][0], coords[0]), Math.max(bounds[1][1], coords[1])]
+                    ]
+                }, [coordinatePairs[0], coordinatePairs[0]]
+            );
+            return [
+                bounds[0].map(function (val) {
+                    return val - margin;
+                }),
+                bounds[1].map(function (val) {
+                    return val + margin;
+                })
+            ]
+        };
+
+        const mapCenter = function(coordinatePairs) {
+            const sums = coordinatePairs
+                .reduce(function(sum, coords) {
+                    return [
+                            (sum[0]) + (coords[0]),
+                            (sum[1]) + (coords[1])
+                    ]
+                }, [0.00, 0.00]
+            );
+            return sums.map(function(sum) {
+                return sum / coordinatePairs.length;
+            });
+        };
+
+        const coordinates = extractCoordinates(posts);
+
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/outdoors-v10',
+            center: mapCenter(coordinates),
+            zoom: 1.5
+        });
+
+
+        map && map.on('load', function () {
+
+            // Add a layer showing the places.
+            map.addLayer({
+                "id": "places",
+                "type": "symbol",
+                "source": {
+                    "type": "geojson",
+                    "data": {
+                        "type": "FeatureCollection",
+                        "features": posts
+                    }
+                },
+                "layout": {
+                    "icon-image": "{icon}-15",
+                    "icon-allow-overlap": true
                 }
-            },
-            "layout": {
-                "icon-image": "{icon}-15",
-                "icon-allow-overlap": true
-            }
+            });
+
+            // there is no option in MapBox to set bounds with no animation
+            // so allow users time to figure out what part of the world is being displayed
+            // without getting too sea sick
+            setTimeout(function(){
+                map.fitBounds(mapBounds(coordinates, 0.11));
+            }, 800);
+
+            // Create a popup, but don't add it to the map yet.
+            var popup = new mapboxgl.Popup({
+                closeButton: true,
+                closeOnClick: false
+            });
+
+            // When a click event occurs on a feature in the places layer, open a popup at the
+            // location of the feature, with description HTML from its properties.
+            map.on('click', 'places', function (e) {
+                var coordinates = e.features[0].geometry.coordinates.slice();
+                var description = e.features[0].properties.description;
+
+                // Ensure that if the map is zoomed out such that multiple
+                // copies of the feature are visible, the popup appears
+                // over the copy being pointed to.
+                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                }
+
+                new mapboxgl.Popup()
+                    .setLngLat(coordinates)
+                    .setHTML(description)
+                    .addTo(map);
+            });
+
+            // Change the cursor to a pointer when the mouse is over the places layer.
+            map.on('mouseenter', 'places', function () {
+                map.getCanvas().style.cursor = 'pointer';
+            });
+
+            // Change it back to a pointer when it leaves.
+            map.on('mouseleave', 'places', function () {
+                map.getCanvas().style.cursor = '';
+            });
         });
 
-        // Create a popup, but don't add it to the map yet.
-        var popup = new mapboxgl.Popup({
-            closeButton: false,
-            closeOnClick: false
+    };
+
+    // limit script to the relevant page
+    $('body').hasClass('posts index') &&
+        $.ajax({
+            url: 'posts.json',
+            dataType: 'text',
+            success: initializeMap
         });
 
-        map.on('mouseenter', 'places', function (e) {
-            // Change the cursor style as a UI indicator.
-            map.getCanvas().style.cursor = 'pointer';
-
-            var coordinates = e.features[0].geometry.coordinates.slice();
-            var description = e.features[0].properties.description;
-
-            // Ensure that if the map is zoomed out such that multiple
-            // copies of the feature are visible, the popup appears
-            // over the copy being pointed to.
-            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-            }
-
-            // Populate the popup and set its coordinates
-            // based on the feature found.
-            popup.setLngLat(coordinates)
-                .setHTML(description)
-                .addTo(map);
-        });
-
-        map.on('mouseleave', 'places', function () {
-            map.getCanvas().style.cursor = '';
-            popup.remove();
-        });
-    });
-
-};
+});
